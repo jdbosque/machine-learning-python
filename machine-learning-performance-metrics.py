be6d974168784813f012a13146d5c35895c15201
Jorge del Bosque <jorge@delbosque.co>
2019-08-23 17:25:00 +0100

    Machine Learning Performance Metrics

---
+++ machine-learning-performance-metrics.py
@@ -0,0 +1,25 @@
+'''
+# Jorge Eugenio del Bosque Trevino
+
+
+Classification Metrics
+Pima Indians Data Set. Scalar features predict a binary feature
+
+Regression Metrics
+All the input variables are numeric (scalar) and the ouput is scalar
+
+Linear Regression: For predict scalar value 
+Logistic Regression : For Classification
+
+Metrics:
+1. Classification Accuracy
+2. Logarithmic Loss
+3. Area Under ROC Curve
+4. Confusion Matrix
+5. Classification Report
+
+'''
+
+
+
+
